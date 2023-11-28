41c3fc1f366ede3267a7e4ab3355eecdfff1414e
jebius76 <91204905+jebius76@users.noreply.github.com>
2023-11-28 09:47:12 -0300

    deploy

--- src/main/java/com/trucking/Security/config/CorsConfig.java
+++ src/main/java/com/trucking/Security/config/CorsConfig.java
@@ -2,8 +2,12 @@ package com.trucking.Security.config;
 
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
@@ -11,9 +15,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/api/v1/**")
-                .allowedOrigins("*")
-                .allowedMethods("*")
-                .exposedHeaders("*")
+                .allowedOrigins("http://localhost:3000","https://trucking-jebius.koyeb.app/**")
+                .allowedMethods("GET","POST","PUT", "DELETE")
+                .allowedHeaders(String.valueOf(Arrays.asList(HttpHeaders.CONTENT_TYPE, HttpHeaders.AUTHORIZATION, HttpHeaders.ACCEPT)))
+                .exposedHeaders("Access-Control-Allow-Origin: https://trucking-jebius.koyeb.app/", "Vary: Origin")
                 .allowCredentials(true);
     }
 }
