ead8d3a8fd30e939af5ef89bfeb32cd3f507deeb
jebius76 <91204905+jebius76@users.noreply.github.com>
2023-12-06 20:35:24 -0300

    deploy

--- src/main/java/com/trucking/Security/Dto/ForgotPasswordDto.java
+++ src/main/java/com/trucking/Security/Dto/ForgotPasswordDto.java
@@ -1,6 +1,7 @@
 package com.trucking.security.dto;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ForgotPasswordDto {
-//    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "El email es invalido")
+    @NotBlank(message = "El email es obligatorio")
     @Email(message = "El email es invalido")
     private String email;
 }
