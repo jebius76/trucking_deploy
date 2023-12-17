02b84841bce6f952728f2c958ae491b08c081e9f
jebius76 <91204905+jebius76@users.noreply.github.com>
2023-12-17 11:49:06 -0300

    deploy nuevo

--- src/main/java/com/trucking/Service/EmailService.java
+++ src/main/java/com/trucking/Service/EmailService.java
@@ -1,13 +1,11 @@
 package com.trucking.service;
 
-import com.trucking.security.dto.ChangePasswordDto;
-import com.trucking.security.dto.DataChangePasswordEmployee;
 import com.trucking.security.dto.DataForgotPasswordDto;
-import com.trucking.security.config.JwtService;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
 import org.springframework.mail.javamail.JavaMailSender;
@@ -20,18 +18,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Service
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class EmailService {
 
+    @Autowired
     private final JavaMailSender javaMailSender;
-    private final TemplateEngine templateEngine;
-    private final JwtService jwtService;
 
+    @Autowired
+    private final TemplateEngine templateEngine;
     @Value("${mail.urlFront}")
     private String urlNewPassword;
-
-    @Value("${mail.loginFront}")
-    private String login;
 
     @Value("${spring.mail.username}")
     private String emailFrom;
@@ -73,30 +69,6 @@ public class EmailService {
             helper.setSubject("Cambiar contraseña de tu cuenta de Trucking");
             helper.setFrom(emailFrom);
             helper.setTo(dataForgotPasswordDto.getEmail());
-            helper.setText(htmlText, true);
-
-            javaMailSender.send(message);
-        } catch (MessagingException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void setEmailWithPassword(DataChangePasswordEmployee employee){
-        MimeMessage message = javaMailSender.createMimeMessage();
-        try {
-            MimeMessageHelper helper = new MimeMessageHelper(message, true);
-            Context context = new Context();
-            Map<String, Object> model = new HashMap<>();
-            model.put("email", emailFrom);
-            model.put("url", login + employee.getToken());
-            model.put("username", employee.getName());
-            model.put("password", employee.getCurrentPassword());
-            context.setVariables(model);
-
-            String htmlText = templateEngine.process("email-template-password", context);
-            helper.setSubject("Accede a tu cuenta en Trucking");
-            helper.setFrom(emailFrom);
-            helper.setTo(employee.getEmail());
             helper.setText(htmlText, true);
 
             javaMailSender.send(message);
