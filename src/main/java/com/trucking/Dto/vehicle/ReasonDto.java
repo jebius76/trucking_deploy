91eb7ca78977be5e3bf80acf3f4da57f2075c64f
jebius76 <91204905+jebius76@users.noreply.github.com>
2023-12-20 10:24:39 -0300

    deploy nuevo

---
+++ src/main/java/com/trucking/Dto/vehicle/ReasonDto.java
@@ -0,0 +1,14 @@
+package com.trucking.dto.vehicle;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ReasonDto {
+
+    private String reason;
+
+}
